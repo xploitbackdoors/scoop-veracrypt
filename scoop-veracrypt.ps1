f421ea76f4156ff0c3492908f562964df39a2d37
xploitbackdoors <129699577+xploitbackdoors@users.noreply.github.com>
2023-04-02 18:42:10 -0500

    Update scoop-veracrypt.ps1

--- scoop-veracrypt.ps1
+++ scoop-veracrypt.ps1
@@ -1,6 +1,6 @@
 {
   "version": "1.25.9",
   "url": "https://launchpad.net/veracrypt/trunk/1.25.9/+download/VeraCrypt_Setup_x64_1.25.9.msi",
-  "extract_dir": "veracrypt",
-  "bin": "veracrypt.ps1"
+  "extract_dir": "scoop-veracrypt-main",
+  "bin": "scoop-veracrypt.ps1"
 }
